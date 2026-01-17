23e78ffcff6f0c7a3b7ada1058cb30a0a228434c
anthony-reis <anthonsareis11@gmail.com>
2026-01-17 14:09:54 -0300

    config: caminhos e formato de imagens

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,28 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "image.tmdb.org",
+        pathname: "/t/p/**",
+      },
+    ],
+    minimumCacheTTL: 60 * 60 * 24 * 7,
 
+    formats: ["image/avif", "image/webp"],
+
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920],
+    imageSizes: [16, 32, 48, 64, 96, 128, 180, 220, 256, 384],
+  },
+
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production",
+  },
+
+  experimental: {
+    optimizePackageImports: ["lucide-react", "@radix-ui/react-icons"],
+  },
+};
 export default nextConfig;
