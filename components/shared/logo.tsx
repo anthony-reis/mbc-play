3a78873a6370284806782c8bbc36c81c89576924
anthony-reis <anthonsareis11@gmail.com>
2026-01-17 08:26:24 -0300

    feat: cria o logo da aplicação

---
+++ components/shared/logo.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+export function Logo() {
+  return (
+    <Link href="/" className="flex items-baseline gap-0 shrink-0">
+      <span className="text-[20px] md:text-[24px] font-black leading-[100%] tracking-[0%]">
+        MBC
+      </span>
+      <span className="text-[18px] md:text-[20px] font-semibold leading-[100%] tracking-[0%] text-red-600">
+        PLAY
+      </span>
+    </Link>
+  );
+}
